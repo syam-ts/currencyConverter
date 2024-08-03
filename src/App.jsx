aaa2542f368a65fc8b3107b8f67016f2d82604d4
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-08-03 20:37:12 +0530

    converter created

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect} from 'react'
+import axios from 'axios'
 import './App.css'
+import CurrencyRow from './CurrencyRow'
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    axios.get(BASE_URL) 
+    .then(res => console.log(res.data))
+    .catch(err => console.log('Error occured', err))
+  },[])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div className="equals"> = </div>
+      <CurrencyRow />
     </>
   )
 }
---
+++ src/CurrencyRow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CurrencyRow() {
+  return (
+    <div> 
+      <input type="number" className="input"/>
+      <select>
+        <option value="Hi">Hi</option>
+      </select>
+    </div>
+  )
+}
+
+export default CurrencyRow
