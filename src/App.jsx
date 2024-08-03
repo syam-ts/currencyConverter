e575a308e3535406d39c2b6896f75b2633b55cc9
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-08-03 21:38:03 +0530

    api implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import { useState , useEffect} from 'react'
-import axios from 'axios'
-import './App.css'
-import CurrencyRow from './CurrencyRow'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import CurrencyRow from './CurrencyRow';
 
-const BASE_URL = 'https://api.exchangeratesapi.io/latest'
-
+const BASE_URL = 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_uywH9iacryultvSyaXVU1ST8vbHdlfii2H4HBs0z';
 
 function App() {
-  useEffect(() => {
-    axios.get(BASE_URL) 
-    .then(res => console.log(res.data))
-    .catch(err => console.log('Error occured', err))
-  },[])
+  
+  const [currencyOptions, setCurrecyOptions] = useState([])
 
+  useEffect(() => {
+    axios.get(BASE_URL)
+      .then(res => {
+        setCurrecyOptions([res.data.base, res.data.data])
+      })
+      .catch(err => console.log('Error occured', err));
+  }, []);
 
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={ currencyOptions }/>
       <div className="equals"> = </div>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={ currencyOptions }/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/CurrencyRow.jsx
+++ src/CurrencyRow.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-function CurrencyRow() {
+function CurrencyRow(props) {
+  const {
+    currencyOptions
+  } = props;
+
+  const [fromcurrecy, setFromCurrecy] = useState()
+  const [tocurrecy, setToCurrecy] = useState()
+
+  console.log('The data : ', currencyOptions);
+
   return (
-    <div> 
+    <div>
       <input type="number" className="input"/>
       <select>
-        <option value="Hi">Hi</option>
+        {currencyOptions.length > 1 && typeof currencyOptions[1] === 'object'
+          ? Object.keys(currencyOptions[1]).map((currencyCode, index) => (
+              <option key={index} value={currencyCode}>{currencyCode}</option>
+            ))
+          : <option value="">No options available</option>
+        }
       </select>
     </div>
-  )
+  );
 }
 
-export default CurrencyRow
+export default CurrencyRow;
